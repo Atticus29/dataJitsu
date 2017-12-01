91758d0682e78882cfb26a9640671af481c2b278
Mark <mark.aaron.fisher@gmail.com>
2017-12-01 10:40:13 -0800

    add isValidWeight method to user model

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,3 +1,7 @@
 export class user {
   constructor(public name: string, public email: string, public rank: string, public affiliation: string, public age: number, public weight: number, public weightClass: string) { }
+
+  private boolean isValidWeight(weight: number){
+    return (number<8 || number > 1400);
+  }
 }
