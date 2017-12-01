6d38a604585e4392adbed0d9274924ff3948b031
Mark <mark.aaron.fisher@gmail.com>
2017-12-01 10:21:44 -0800

    add match model not fleshed out

---
+++ src/app/match.model.ts
@@ -0,0 +1,3 @@
+export class match {
+  constructor(public name: string, public position: string, public jerseyNumber: number, public height: number, public weight: number, public birthdate: string, public imageURL: string) { }
+}
