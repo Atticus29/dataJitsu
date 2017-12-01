e4b627fdf5cd514fd237ed5a7947506c8c09d827
Mark <mark.aaron.fisher@gmail.com>
2017-12-01 10:11:24 -0800

    do what was recommended in the stack overflow question https://stackoverflow.com/questions/43508040/record-how-far-into-a-video-a-user-hits-the-pause-button. Error about YT

--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -10,6 +10,37 @@ export class YoutubeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var player;
+
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('video', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+
+    function onPlayerStateChange(event){
+      if (event.data == YT.PlayerState.PAUSED) {
+        console.log(player.getCurrentTime());
+      }
+    }
+
+    function onPlayerReady(event) {
+      document.getElementById("pause").addEventListener("click", function() {
+        player.pauseVideo();
+      });
+      document.getElementById("play").addEventListener("click", function() {
+        player.playVideo();
+      });
+    }
+
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
   }
 
 }
