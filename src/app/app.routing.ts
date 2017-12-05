571ed2fd076c39f985624f01f0f98bde3a1f574a
Mark <mark.aaron.fisher@gmail.com>
2017-12-04 16:44:32 -0800

    add newmatch to routing and confirm that it works

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,18 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { YoutubeComponent } from './youtube/youtube.component';
+import { NewMatchComponent } from './new-match/new-match.component'
 
 const appRoutes: Routes = [
   {
   path: '',
-  component: YoutubeComponent
-  },
+  component: YoutubeComponent,
+  pathMatch: 'full'
+},{
+  path: 'newmatch',
+  component: NewMatchComponent,
+  pathMatch: 'full'
+}
  ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
