719bb5fdd377c5159f896d9f99c8aea40f9a6eb9
Mark <mark.aaron.fisher@gmail.com>
2017-12-04 17:30:19 -0800

    install materialize css and materialize correctly as per https://www.npmjs.com/package/angular2-materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 import { YoutubeComponent } from './youtube/youtube.component';
@@ -18,7 +19,8 @@ import { NewMatchComponent } from './new-match/new-match.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
