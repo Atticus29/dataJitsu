6cc5755e7bc2b5a5eeb9786c49ff021ef832462d
Mark <mark.aaron.fisher@gmail.com>
2017-12-05 16:23:45 -0800

    resolve modal issue by declaring $ as any

--- src/app/new-match/new-match.component.ts
+++ src/app/new-match/new-match.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import {MaterializeDirective,MaterializeAction} from "angular2-materialize";
 // import { ViewChild, ElementRef, AfterViewInit} from '@angular/core';
-// declare var jQuery:JQueryStatic;
+declare var $:any;
 
 @Component({
   selector: 'app-new-match',
@@ -9,46 +9,30 @@ import {MaterializeDirective,MaterializeAction} from "angular2-materialize";
   styleUrls: ['./new-match.component.css']
 })
 export class NewMatchComponent implements OnInit {
-  modalActions1 = new EventEmitter<string|MaterializeAction>();
-  modalActions2 = new EventEmitter<string|MaterializeAction>();
 
-  params = []
+constructor() {
+  $(document).ready(function() {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    /*$('.view').click(function (){
+    $('#modal1').modal('open');
+    alert('edskjcxnm');
+  });*/
+  /*$('.view').leanModal();*/
+  $('#modal1').modal();
+});
+}
 
-  model1Params = [
-    {
-      dismissible: false,
-      complete: () => {
-        console.log('Closed');
-        this.openModal2();
-      }
-    }
-  ]
+ngOnInit() {
+}
 
-  constructor() {}
 
-  ngOnInit() {
-  }
-
-  openModal1() {
-    this.modalActions1.emit({action:"modal",params:['open']});
-  }
-  closeModal1() {
-    this.modalActions1.emit({action:"modal",params:['close']});
-  }
-  openModal2() {
-    this.modalActions2.emit({action:"modal",params:['open']});
-  }
-  closeModal2() {
-    this.modalActions2.emit({action:"modal",params:['close']});
-  }
-
-  submitForm(){
-    console.log("Hi!");
-    //push results to database(match) (as service??) //TODO
-    //send a little dialog box
-    //if they chose to annotate, go to annotation page
-    //else
-    //go back to main page
-  }
+submitForm(){
+  console.log("Hi!");
+  //push results to database(match) (as service??) //TODO
+  //send a little dialog box
+  //if they chose to annotate, go to annotation page
+  //else
+  //go back to main page
+}
 
 }
