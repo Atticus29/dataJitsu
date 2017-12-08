b56189724a72fdf25492f9411ce14bfc3bcd4ecc
Mark <mark.aaron.fisher@gmail.com>
2017-12-07 16:01:35 -0800

    move modal initialization from the constructor to ngOnInit for appropriate modal behavior

--- src/app/new-match/new-match.component.ts
+++ src/app/new-match/new-match.component.ts
@@ -16,13 +16,10 @@ export class NewMatchComponent implements OnInit {
   weightClasses: Array<string>;
 
   constructor() {
-    $(document).ready(function() {
-      //TODO fix bug where navigating from main to newmatch and then clicking the modal submit sometimes works and sometimes doesn't (depending on whether you've navigated away from the page before??)
-      $('.modal').modal();
-    });
   }
 
   ngOnInit() {
+    $('.modal').modal();
     this.genders = ["Female", "Male"];
     this.ages = ["Youth", "Juvenile1", "Juvenile2", "Adult", "Master 1", "Master 2", "Master 3", "Master 4", "Master 5", "Master 6"];
     this.ranks = ["White belt", "Grey belt", "Yellow belt", "Orange belt", "Green belt", "Blue belt", "Purple belt", "Brown belt", "Black belt", "Beginner", "Intermediate", "Advanced", "Elite"];
