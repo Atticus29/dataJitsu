47742b4ef6b7754cd45d25d0cb5bee934d38bec6
Mark <mark.aaron.fisher@gmail.com>
2017-12-14 15:48:24 -0800

    add firebase imports (and fleshout out the api-keys file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
-
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { YoutubeComponent } from './youtube/youtube.component';
 import { routing } from './app.routing';
 import { NewMatchComponent } from './new-match/new-match.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +30,9 @@ import { NewMatchComponent } from './new-match/new-match.component';
     HttpModule,
     routing,
     MaterializeModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
