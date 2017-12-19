87ab661e4dfe32cdc13540ccf34c9bfcefe8a575
Mark <mark.aaron.fisher@gmail.com>
2017-12-18 23:18:37 -0800

    begin working the back end of the new User form and remove id from User constructor and make setter

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../database.service';
 import { Subject } from 'rxjs/Subject';
+import { FormBuilder, FormGroup, FormControl, FormArray, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -10,17 +11,33 @@ import { Subject } from 'rxjs/Subject';
 })
 export class CreateAccountComponent implements OnInit {
   private ngUnsubscribe: Subject<void> = new Subject<void>();
+  newUserForm: FormGroup;
   title: string = "Create an Account";
   genders: Array<string> = ["Female", "Male"];
   weightClasses: any[];
   noGiRanks: any[];
   giRanks: any[];
   ageClasses: any[];
+  ages: Array<number>;
 
 
-  constructor(private db: DatabaseService) { }
+  constructor(private fb: FormBuilder, private db: DatabaseService) { }
 
   ngOnInit() {
+    for (var i = 3; i <= 110; i++) {
+       this.ages.push(i);
+    }
+    this.newUserForm = this.fb.group({
+      userNameBound: ['', Validators.required],
+      userEmailBound: ['', Validators.required],
+      genderBound: ['', Validators.required],
+      ageClassBound: ['', Validators.required],
+      giRankBound: ['', Validators.required],
+      noGiRankBound: ['', Validators.required],
+      weightClassBound: ['', Validators.required],
+      ageBound: ['', Validators.required]
+    });
+
     this.db.getGiRanks().takeUntil(this.ngUnsubscribe).subscribe(giRanks=>{
       this.giRanks = giRanks;
     })
@@ -38,12 +55,26 @@ export class CreateAccountComponent implements OnInit {
     });
   }
 
+  getValues(){
+    let result = this.newUserForm.value;
+    return result;
+  }
+
+  createUserObj(result: any){
+    let {userNameBound, userEmailBound, genderBound, ageClassBound, giRankBound, noGiRankBound, weightClassBound, ageBound} = result;
+    let newUser = new User()
+  }
+
   allValid(){
-    return true; //TODO fix
+    console.log("entered allValid");
+    let values = this.newUserForm.value;
+    console.log(values);
+    if(values.userNameBound !== "" && values.userEmailBound !== "" && values.genderBound !== "" && values.ageClassBound !== "" && values.weightClassBound !== "" && values.giRankBound !== "" && values.noGiRankBound !== "" && values.ageBound !== ""){
+      console.log("allValid valid");
+      return true;
+    } else{
+      console.log("allValid not valid");
+      return false;
+    }
   }
-
-  createUser(){
-
-  }
-
 }
