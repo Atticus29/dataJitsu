17a1e38c38860344c0586de1255b601a57831c64
Mark <mark.aaron.fisher@gmail.com>
2018-08-05 18:40:05 -0700

    add what I thought was a fix to the hidden options issues, but it seems nothing is ever selected

--- cypress/integration/examples/sample.spec.js
+++ cypress/integration/examples/sample.spec.js
@@ -25,18 +25,43 @@ describe ('Firestarter', () =>{
 
   it('signs up a new user', () =>{
     cy.get('button[id=new-account-button]').click();
-    cy.get('[name="test"]').click();
-    cy.get('select[id=ageClass]').select(ageClass);
-    cy.get('input[id=weight]').type(weight);
-    cy.get('select[id=noGiRank]').select(noGiRank);
-    cy.get('select[id=giRank]').select(giRank);
-    cy.get('select[id=ageClass]').select(ageClass);
     cy.get('input[id=affiliation]').type(affiliation);
     cy.get('input[id=password]').type(pass);
     cy.get('input[id=userName]').type(name);
     cy.get('input[id=email]').type(email);
+
+    //Now the options
+    cy.get('[name="chooseGender"]').click({force: true});
+    cy.get('option').contains("Female").then(option =>{
+      cy.wrap(option).contains("Female");
+      console.log(option);
+      // option[1].click();
+    });
+
+    cy.get('[name=chooseAgeClass]').click({force: true});
+    cy.get('option').contains("Adult").then(option =>{
+      cy.wrap(option).contains("Adult");
+      option[0].click();
+    });
+    cy.get('input[id=weight]').type(weight);
+    cy.get('[name=chooseNoGiRank]').click({force: true});
+    cy.get('option').contains("Elite").then(option =>{
+      cy.wrap(option).contains("Elite");
+      option[0].click();
+    });
+
+    cy.get('[name=chooseGiRank]').click({force: true});
+    cy.get('option').contains("Black belt").then(option =>{
+      cy.wrap(option).contains("Black belt");
+      option[0].click();
+    })
+    cy.get('[name="chooseAge"]').click({force: true});
+    cy.get('option').contains("27").then(option =>{
+      cy.wrap(option).contains("27");
+      option[0].click();
+    });
     cy.get('button[id=create-button]').click();
-    cy.contains('Rank'); //TODO make this something more permanent and inside the fixtures directory
+    // cy.contains('Rank'); //TODO make this something more permanent and inside the fixtures directory
   })
 
   it('blocks protected routes', () =>{
