d4aff48eaa47441ddce88744efa569284922c374
Mark <mark.aaron.fisher@gmail.com>
2019-01-18 17:29:36 -0800

    maybe fixed lack of api-keys.ts now?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { masterFirebaseConfig } from './api-keys';
+// import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
@@ -72,10 +72,10 @@ import {
 
 
 export const firebaseConfig = {
-  apiKey: masterFirebaseConfig.apiKey,
-  authDomain: masterFirebaseConfig.authDomain,
-  databaseURL: masterFirebaseConfig.databaseURL,
-  storageBucket: masterFirebaseConfig.storageBucket
+  apiKey: $fireBaseApiKey, //masterFirebaseConfig.apiKey,
+  authDomain: 'dataJitsu.firebaseapp.com', //masterFirebaseConfig.authDomain,
+  databaseURL: 'https://datajitsu.firebaseio.com',//masterFirebaseConfig.databaseURL,
+  storageBucket: '', //masterFirebaseConfig.storageBucket
 };
 
 @NgModule({
