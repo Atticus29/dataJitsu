18a9cddb82c1f6f268ed1981f44b407c7294abf0
Mark <mark.aaron.fisher@gmail.com>
2019-01-18 17:50:57 -0800

    remove stripe stuff

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,2 +1,1 @@
 export var masterFirebaseConfig = {apiKey: fireBaseApiKey, authDomain: 'dataJitsu.firebaseapp.com',databaseURL: 'https://datajitsu.firebaseio.com',storageBucket: '',messagingSenderId: '495992924984'};
-export var masterStripeConfig = {publicApiTestKey: masterStripePublicApiKey,secretApiTestKey: masterStripeSecretApiKey,publicApiKey: '',secretApiKey: ''};
