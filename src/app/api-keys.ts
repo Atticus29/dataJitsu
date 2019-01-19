ddef91e18836a084ae92da79c2f331e5a714f60e
Mark <mark.aaron.fisher@gmail.com>
2019-01-18 17:40:47 -0800

    freeze

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,2 +1,2 @@
-export var masterFirebaseConfig = {apiKey: $fireBaseApiKey, authDomain: 'dataJitsu.firebaseapp.com',databaseURL: 'https://datajitsu.firebaseio.com',storageBucket: '',messagingSenderId: '495992924984'};
-export var masterStripeConfig = {publicApiTestKey: $masterStripePublicApiKey,secretApiTestKey: $masterStripeSecretApiKey,publicApiKey: '',secretApiKey: ''};
+export var masterFirebaseConfig = {apiKey: fireBaseApiKey, authDomain: 'dataJitsu.firebaseapp.com',databaseURL: 'https://datajitsu.firebaseio.com',storageBucket: '',messagingSenderId: '495992924984'};
+export var masterStripeConfig = {publicApiTestKey: masterStripePublicApiKey,secretApiTestKey: masterStripeSecretApiKey,publicApiKey: '',secretApiKey: ''};
