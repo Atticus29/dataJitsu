2b5f9543e119cbe2f3a868a739deec3bfab332d7
Mark <mark.aaron.fisher@gmail.com>
2019-02-03 21:02:21 -0800

    restore to old level of failure

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -16,7 +16,7 @@ describe ('Firestarter', () =>{
   const affiliation = 'Straight Blast Gym';
 
   beforeEach(()=>{
-    cy.visit('http-get://localhost:4200');
+    cy.visit('/');
   });
 
   it.skip('logs out intially might fails', ()=>{
