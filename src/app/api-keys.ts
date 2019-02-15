9ad380ba4a7811de7055c6622d1b15e23aa74372
Mark <mark.aaron.fisher@gmail.com>
2019-02-14 16:38:51 -0800

    add physical api keys back in and limit to known passing tests for testing purposes

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,1 +1,14 @@
-export var masterFirebaseConfig = {apiKey: process.env.$fireBaseApiKey, authDomain: 'dataJitsu.firebaseapp.com',databaseURL: 'https://datajitsu.firebaseio.com',storageBucket: '',messagingSenderId: '495992924984'};
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyCaYbzcG2lcWg9InMZdb10pL_3d1LBqE1A",
+    authDomain: "dataJitsu.firebaseapp.com",
+    databaseURL: "https://datajitsu.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "495992924984"
+  };
+
+export var masterStripeConfig = {
+  publicApiTestKey: "pk_test_NKyjLSwnMosdX0mIgQaRRHbS",
+  secretApiTestKey: "sk_test_6YWZDNhzfMq3UWZwdvcaOwSa",
+  publicApiKey: "",
+  secretApiKey: ""
+};
