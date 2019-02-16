4e52dd09c89d4c8ae6bf67cd73c6010b07316318
Mark <mark.aaron.fisher@gmail.com>
2019-02-15 21:21:30 -0800

    change quotes in api-keys.ts

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,5 +1,5 @@
 export var masterFirebaseConfig = {
-    apiKey: circleCiApiKey,
+    apiKey: "circleCiApiKey",
     authDomain: "dataJitsu.firebaseapp.com",
     databaseURL: "https://datajitsu.firebaseio.com",
     storageBucket: "",
