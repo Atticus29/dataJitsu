402cc6214ab81be1f34e754dfb74153c4fe6180c
Mark <mark.aaron.fisher@gmail.com>
2019-07-20 20:58:40 -0700

    fix Object.entries(jsonStr) error by updating js version in tsconfig and add api-key file with circleCI variable in place to see whether it will work on circleCI now

---
+++ src/app/api-keys.ts
@@ -0,0 +1,14 @@
+export var masterFirebaseConfig = {
+    apiKey: "circleCiApiKey",
+    authDomain: "dataJitsu.firebaseapp.com",
+    databaseURL: "https://datajitsu.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "495992924984"
+  };
+
+export var masterStripeConfig = {
+  publicApiTestKey: "pk_test_NKyjLSwnMosdX0mIgQaRRHbS",
+  secretApiTestKey: "sk_test_6YWZDNhzfMq3UWZwdvcaOwSa",
+  publicApiKey: "",
+  secretApiKey: ""
+};
