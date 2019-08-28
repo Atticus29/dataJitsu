16bbaea159ec5ee7ffee3b25b7cb69e40f087af4
Mark <mark.aaron.fisher@gmail.com>
2019-08-28 12:11:26 -0700

    table freeze before version change

--- src/app/all-matches/all-matches.component.ts
+++ src/app/all-matches/all-matches.component.ts
@@ -41,18 +41,61 @@ import { MoveInVideo } from '../moveInVideo.model';
 })
 export class AllMatchesComponent implements OnInit, OnDestroy {
   private columnsToDisplay = ['rank','weightClass', 'ageClass','athlete1Name', 'athlete2Name', 'gender','tournamentName','location', 'date', 'matchRating', 'annotationRating','videoUrl']; //TODO make this dynamic somehow
-  private loading = true;
-  private showLoader: any;
+  // private loading = true;
+  // private showLoader: any;
   user: any = null;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
-  private matchCount: number;
-  private pageSize: number;
-  private dataSource: MatTableDataSource<Match>
+  // private matchCount: number;
+  // private pageSize: number;
+  dataSource: MatTableDataSource<Match>
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor(private d3Service: D3Service, private dbService: DatabaseService, private textTransformationService: TextTransformationService, private cdr: ChangeDetectorRef, private router: Router, private trackerService: TrackerService) { }
+  constructor(private d3Service: D3Service, private dbService: DatabaseService, private textTransformationService: TextTransformationService, private cdr: ChangeDetectorRef, private router: Router, private trackerService: TrackerService) {
+    let matchDeets1: MatchDetails = new MatchDetails("IBJJF Worlds", "los angeles, california", new Date().toJSON(), "athlete1", "athlete2", "rooster", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets2: MatchDetails = new MatchDetails("ADCC", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets3: MatchDetails = new MatchDetails("JKLM", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets4: MatchDetails = new MatchDetails("Abu Dabi", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets5: MatchDetails = new MatchDetails("NAGA", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets6: MatchDetails = new MatchDetails("SubOnly", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets7: MatchDetails = new MatchDetails("Mom", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets8: MatchDetails = new MatchDetails("Dad", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets9: MatchDetails = new MatchDetails("Brother", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+    let matchDeets10: MatchDetails = new MatchDetails("Sister", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
+
+    let move1: MoveInVideo = new MoveInVideo("kimura", "athlete1", "athlete2", 365, 389, 0, "testMachId", true, true, "annotatorId");
+    let move2: MoveInVideo = new MoveInVideo("back mount", "athlete1", "athlete2", 350, 363, 4, "testMachId2", false, true, "annotatorId2");
+    let moveArray: Array<MoveInVideo> = [move1, move2];
+    let now: Date = new Date();
+    // let user1: User = new User("testUser", "bob@bob.com","1234567", "black belt", "expert", "sbg", 33, 155, 100, now.toString(), false, "male", now.toString());
+    let match1: Match = new Match(matchDeets1, "originalPosterId", moveArray);
+    let match2: Match = new Match(matchDeets2, "originalPosterId", moveArray);
+    let match3: Match = new Match(matchDeets3, "originalPosterId", moveArray);
+    let match4: Match = new Match(matchDeets4, "originalPosterId", moveArray);
+    let match5: Match = new Match(matchDeets5, "originalPosterId", moveArray);
+    let match6: Match = new Match(matchDeets6, "originalPosterId", moveArray);
+    let match7: Match = new Match(matchDeets7, "originalPosterId", moveArray);
+    let match8: Match = new Match(matchDeets8, "originalPosterId", moveArray);
+    let match9: Match = new Match(matchDeets9, "originalPosterId", moveArray);
+    let match10: Match = new Match(matchDeets10, "originalPosterId", moveArray);
+
+    let matchArray: Array<Match> = new Array<Match>();
+    matchArray.push(match1);
+    matchArray.push(match2);
+    matchArray.push(match3);
+    matchArray.push(match4);
+    matchArray.push(match5);
+    matchArray.push(match6);
+    matchArray.push(match7);
+    matchArray.push(match8);
+    matchArray.push(match10);
+
+    console.log(matchArray);
+
+    // this.pageSize = 5;
+    this.dataSource = new MatTableDataSource(matchArray);
+  }
 
   ngOnInit() {
     this.trackerService.currentUserBehaviorSubject.pipe(takeUntil(this.ngUnsubscribe)).subscribe(user=>{
@@ -73,27 +116,12 @@ export class AllMatchesComponent implements OnInit, OnDestroy {
       }
     });
 
-    let matchDeets1: MatchDetails = new MatchDetails("IBJJF Worlds", "los angeles, california", new Date().toJSON(), "athlete1", "athlete2", "rooster", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
-    let matchDeets2: MatchDetails = new MatchDetails("ADCC", "los angeles, california", new Date().toJSON(), "mendes", "gracie", "feather", "black", "https://www.youtube.com/watch?v=LPj368_plK0&index=183&list=WL", "male", false, "master 1");
-    let move1: MoveInVideo = new MoveInVideo("kimura", "athlete1", "athlete2", 365, 389, 0, "testMachId", true, true, "annotatorId");
-    let move2: MoveInVideo = new MoveInVideo("back mount", "athlete1", "athlete2", 350, 363, 4, "testMachId2", false, true, "annotatorId2");
-    let moveArray: Array<MoveInVideo> = [move1, move2];
-    let now: Date = new Date();
-    // let user1: User = new User("testUser", "bob@bob.com","1234567", "black belt", "expert", "sbg", 33, 155, 100, now.toString(), false, "male", now.toString());
-    let match1: Match = new Match(matchDeets1, "originalPosterId", moveArray);
-    let match2: Match = new Match(matchDeets2, "originalPosterId", moveArray);
-    let matchArray: Array<Match> = new Array<Match>();
-    matchArray.push(match1);
-    matchArray.push(match2);
-
-    this.pageSize = 10;
-    this.dataSource = new MatTableDataSource(matchArray);
     // this.dataSource = new MatchDataSource(this.dbService);
 
     // this.dataSource.loadMatches('test', '', '', 0, this.pageSize);
-    this.dbService.getMatchCount().subscribe(results=>{
-      this.matchCount = results;
-    });
+    // this.dbService.getMatchCount().subscribe(results=>{
+    //   this.matchCount = results;
+    // });
     // this.dataSource.loading$.subscribe(result =>{
     //   this.showLoader = result;
     //   this.cdr.detectChanges();
@@ -138,6 +166,7 @@ export class AllMatchesComponent implements OnInit, OnDestroy {
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource.filter);
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
