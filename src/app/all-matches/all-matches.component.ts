624cdb4380f0d610088e3b9b4646d59f3fa6e9c4
Mark Fisher <mark.aaron.fisher@gmail.com>
2019-09-08 20:09:16 -0700

    add filterPredicate per advice here https://stackoverflow.com/questions/57700078/angular-material-mat-table-not-returning-any-results-upon-filter/57839529#57839529 to get table to filter successfully

--- src/app/all-matches/all-matches.component.ts
+++ src/app/all-matches/all-matches.component.ts
@@ -102,6 +102,16 @@ export class AllMatchesComponent implements OnInit {
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    //set a new filterPredicate function
+   this.dataSource.filterPredicate = (data, filter: string) => {
+     const accumulator = (currentTerm, key) => {
+       return this.nestedFilterCheck(currentTerm, data, key);
+     };
+     const dataStr = Object.keys(data).reduce(accumulator, '').toLowerCase();
+     // Transform the filter by converting it to lowercase and removing whitespace.
+     const transformedFilter = filter.trim().toLowerCase();
+     return dataStr.indexOf(transformedFilter) !== -1;
+   }
 
     this.trackerService.currentUserBehaviorSubject.pipe(takeUntil(this.ngUnsubscribe)).subscribe(user=>{
       this.user = user;
@@ -135,6 +145,19 @@ export class AllMatchesComponent implements OnInit {
     // this.dbService.getMatches().subscribe(results =>{
     //   this.loadMatchesPage();
     // });
+  }
+
+  nestedFilterCheck(search, data, key) {
+    if (typeof data[key] === 'object') {
+      for (const k in data[key]) {
+        if (data[key][k] !== null) {
+          search = this.nestedFilterCheck(search, data[key], k);
+        }
+      }
+    } else {
+      search += data[key];
+    }
+    return search;
   }
 
   // ngAfterViewInit(){
