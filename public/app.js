6c0be8a064ea69aa5c3f5bf5a354771370c8d2d6
Mark <mark.aaron.fisher@gmail.com>
2019-10-04 13:46:59 -0700

    get the function callback to work

--- public/app.js
+++ public/app.js
@@ -1,45 +1,50 @@
-// require("firebase/functions"); Got this idea from SO, but doesn't recognize require
-
 document.addEventListener('DOMContentLoaded', function() {
 
-  try {
-    let app = firebase.app();
-    let features = ['auth', 'functions'].filter(feature => typeof app[feature] === 'function');
-    console.log("Firebase SDK loaded with ${features.join(', ')}");
-    // document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
-  } catch (e) {
-    console.error(e);
-    // document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-  }
+  // Initialize
+  let app = firebase.app();
+  let features = ['auth', 'functions'].filter(
+    feature => typeof app[feature] === 'function'
+  );
+  console.log(`Firebase SDK loaded with ${features.join(', ')}`);
 
+  // Firebase Services
   const fun = firebase.functions();
   const auth = firebase.auth();
+
+  // DOM Elements
   const loginBtn = document.getElementById('login');
   const logoutBtn = document.getElementById('logout');
   const profile = document.getElementById('profile');
 
+  // Realtime listener for Auth State
   auth.onAuthStateChanged(user => {
-    if (user){
-      profile.innerHTML = user.uid;
-      loginBtn.style.visibility = 'hidden';
-      logoutBtn.style.visibility = 'visbile';
-    } else{
-      profile.innerHTML = "not logged in";
-      loginBtn.style.visibility = 'visible';
-      logoutBtn.style.visibility = 'hidden';
+
+    if (user) {
+        profile.innerHTML = user.uid;
+        loginBtn.style.visibility = 'hidden';
+        logoutBtn.style.visibility = 'visible';
+    } else {
+        profile.innerHTML = 'not logged in';
+        loginBtn.style.visibility = 'visible';
+        logoutBtn.style.visibility = 'hidden';
     }
+
   });
 
-  //Event Handlers
+  // Event Handlers
+
   loginBtn.onclick = () => auth.signInAnonymously();
   logoutBtn.onclick = () => auth.signOut();
 
-  //Callable functions
+
+
+  // Callable Functions
   const testFun = fun.httpsCallable('testFunction');
   const testFunButton = document.getElementById('testFunButton');
 
   document.getElementById('testFunButton').onclick = async () => {
-    const response = await testFun({message: "Howdy!"});
+    const response = await testFun({ message: 'Howdy!' });
+
     console.log(response);
   };
 });
