f42b5d07535b21c6c2fe657463e9296eaef48d07
Mark <mark.aaron.fisher@gmail.com>
2019-10-12 20:24:28 -0700

    minor fix to test and we try again

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -19,8 +19,8 @@ Cypress.Commands.add("fillInMatchCreationDetails", (email, pass) => {
     cy.get('mat-select[id=athlete2-select]').click();
     cy.get('mat-option').first().next().next().next().click({force: true});
     cy.get('input[id=tournamentName]').click({force:true}).clear().type(cypressConstants.testTournament);
-    cy.get('input[id=location]').clear().type(cypressConstants.testLocation);
-    cy.get('input[id=date-input]').clear().type(cypressConstants.testDate);
+    cy.get('input[id=location]')click({force:true}).clear().type(cypressConstants.testLocation);
+    cy.get('input[id=date-input]').click({force: true}).clear().type(cypressConstants.testDate);
     cy.get('mat-select[id=gender-select]').click();
     cy.get('mat-option').first().next().click({force:true});
     cy.get('mat-select[id=ageClass]').click();
