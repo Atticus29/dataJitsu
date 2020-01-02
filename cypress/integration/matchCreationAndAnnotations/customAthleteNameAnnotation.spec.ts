ac7f3f7ee2e0569d0887f3909e6d260f2a74be9b
Mark <mark.aaron.fisher@gmail.com>
2020-01-01 22:27:08 -0800

    update cypress and add second deleteMatch to customAthleteName test set in hopes it will fix errors downstream

--- cypress/integration/matchCreationAndAnnotations/customAthleteNameAnnotation.spec.ts
+++ cypress/integration/matchCreationAndAnnotations/customAthleteNameAnnotation.spec.ts
@@ -69,6 +69,8 @@ describe ('Match custom athlete name annotation tests', () =>{
     cy.fixture('cypressConstants.json').then((cypressConstants)=>{
       cy.log("delete this match");
       cy.deleteMatch(cypressConstants.testLocation);
+      //And maybe again because it looks like there's 2?
+      cy.deleteMatch(cypressConstants.testLocation);
     });
     // cy.logout();
     // cy.loginAsAdmin();
